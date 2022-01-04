50f0855dc63848e8337a398f4dcba7fd424b855d
Parsec <sailcali@gmail.com>
2022-01-04 15:06:33 -0800

    added second pin for local temp

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ GARAGE_IP = os.environ.get("GARAGE_IP")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO_PIN = 17
-GPIO.setup(GPIO_PIN, GPIO.OUT)
+LIVING_RM_PIN = 17
+LOCAL_PIN = 27
+GPIO.setup(LIVING_RM_PIN, GPIO.OUT)
+GPIO.setup(LOCAL_PIN, GPIO.OUT)
 
 temp_response = requests.get(f'http://{SERVER_IP}/api/current_temps')
 current_temps = temp_response.json()
@@ -20,8 +22,15 @@ garage_response = requests.get(f'http://{GARAGE_IP}/get-status')
 garage_temps = garage_response.json()
 
 if current_temps['living_room_temp'] <= garage_temps['temperature']:
-    if not GPIO.input(GPIO_PIN):
-        GPIO.output(GPIO_PIN, GPIO.HIGH)
+    if not GPIO.input(LIVING_RM_PIN):
+        GPIO.output(LIVING_RM_PIN, GPIO.HIGH)
 else:
-    if GPIO.input(GPIO_PIN):
-        GPIO.output(GPIO_PIN, GPIO.LOW)
+    if GPIO.input(LIVING_RM_PIN):
+        GPIO.output(LIVING_RM_PIN, GPIO.LOW)
+
+if current_temps['thermostat_temp'] <= garage_temps['temperature']:
+    if not GPIO.input(LOCAL_PIN):
+        GPIO.output(LOCAL_PIN, GPIO.HIGH)
+else:
+    if GPIO.input(LOCAL_PIN):
+        GPIO.output(LOCAL_PIN, GPIO.LOW)
