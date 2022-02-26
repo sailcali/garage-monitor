7173f4d9e55c5ed38b2f02478f597af81a2c6268
Parsec <sailcali@gmail.com>
2022-02-25 19:21:52 -0800

    added api call for outdoor temps

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x77)
 local_farenheight = bme280.temperature * (9/5) + 32
 def setup():
 	LCD1602.init(0x27, 1)	# init(slave address, background light)
-	LCD1602.write(0, 0, 'Garage     Local')
-	LCD1602.write(0, 1, f"{int(garage_temps['temperature'])}*F     {int(bme280.humidity)}%{int(local_farenheight)}*F")
+	LCD1602.write(0, 0, 'Outdoor   Local')
+	LCD1602.write(0, 1, f"{int(remote_temp)}*F     {int(bme280.humidity)}%{int(local_farenheight)}*F")
 	time.sleep(2)
 
 def destroy():
@@ -26,6 +26,8 @@ print(bme280.humidity)
 load_dotenv()
 SERVER_IP = os.environ.get("SERVER_IP")
 GARAGE_IP = os.environ.get("GARAGE_IP")
+VENSTAR_IP = os.environ.get("VENSTAR_IP")
+VENSTAR_SENSOR_URL = 'http://' + VENSTAR_IP + '/query/sensors'
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -38,6 +40,13 @@ temp_response = requests.get(f'http://{SERVER_IP}/api/current_temps')
 current_temps = temp_response.json()
 garage_response = requests.get(f'http://{GARAGE_IP}/get-status')
 garage_temps = garage_response.json()
+sensor_response = requests.get(VENSTAR_SENSOR_URL)
+sensors = sensor_response.json()
+
+remote_temp = 'N/A' # set to N/A in case its not found!
+for sensor in sensors['sensors']:
+    if sensor['name'] == 'Remote':
+        remote_temp = sensor['temp']
 
 if current_temps['living_room_temp'] <= garage_temps['temperature']:
     if not GPIO.input(LIVING_RM_PIN):
